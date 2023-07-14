ca6cc4d1337145bc6fa526cc3ccc3f51d4e914b8
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-07-14 17:52:21 -0300

    video 2.2

--- src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
@@ -1,9 +1,15 @@
 package br.com.alura.alugames.modelo
 
+import java.time.LocalDate
+import java.time.Period
+
 data class Aluguel(
     val gamer: Gamer,
-    val jogo: Jogo) {
+    val jogo: Jogo,
+    val dataInicial: LocalDate,
+    val dataFinal: LocalDate) {
+    val valorDoAluguel = jogo.preco * Period.between(dataInicial, dataFinal).days
     override fun toString(): String {
-        return "Aluguel do jogo ${jogo.titulo} por ${gamer.nome}"
+        return "Aluguel do jogo ${jogo.titulo} por ${gamer.nome} pelo valor R$$valorDoAluguel"
     }
 }
--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.alura.alugames.modelo
 
+import java.time.LocalDate
 import java.util.Scanner
 import kotlin.random.Random
 
@@ -50,8 +51,8 @@ data class Gamer(var nome:String, var email:String) {
         }
     }
 
-    fun alugaJogo(jogo: Jogo): Aluguel {
-        return Aluguel(this, jogo)
+    fun alugaJogo(jogo: Jogo, dataInicial: LocalDate, dataFinal: LocalDate): Aluguel {
+        return Aluguel(this, jogo, dataInicial, dataFinal)
     }
 
     companion object {
