092645007d0d22029a44f1d46d0c14744983e37e
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-07-14 20:48:48 -0300

    Faça como eu fiz: vamos praticar?

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -59,6 +59,12 @@ data class Gamer(var nome:String, var email:String) {
         return aluguel
     }
 
+    fun jogosDoMes(mes:Int): List<Jogo> {
+        return jogosAlugados
+            .filter { aluguel ->  aluguel.periodo.dataInicial.monthValue == mes}
+            .map { aluguel ->  aluguel.jogo}
+    }
+
     companion object {
         fun criarGamer(leitura: Scanner): Gamer {
             println("Boas vindas ao AluGames! Vamos fazer seu cadastro. Digite seu nome:")
