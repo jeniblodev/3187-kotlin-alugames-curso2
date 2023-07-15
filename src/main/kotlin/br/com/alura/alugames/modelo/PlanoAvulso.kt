822acc353e5d3cdfad840ce952380b3d82a49fda
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-07-14 23:53:51 -0300

    video 4.3

--- src/main/kotlin/br/com/alura/alugames/modelo/PlanoAvulso.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/PlanoAvulso.kt
@@ -3,5 +3,11 @@ package br.com.alura.alugames.modelo
  class PlanoAvulso(
     tipo: String): Plano(tipo) {
 
-
+     override fun obterValor(aluguel: Aluguel): Double {
+         var valorOriginal = super.obterValor(aluguel)
+         if (aluguel.gamer.media > 8) {
+             valorOriginal -= valorOriginal * 0.1
+         }
+         return valorOriginal
+     }
 }
--- src/main/kotlin/br/com/alura/alugames/principal/testeJson.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/testeJson.kt
@@ -31,7 +31,7 @@ fun main() {
 //    println(gamerCaroline.jogosAlugados)
 
     val gamerCamila = listaGamers.get(5)
-    gamerCamila.plano = PlanoAssinatura("PRATA", 9.90, 3)
+//    gamerCamila.plano = PlanoAssinatura("PRATA", 9.90, 3)
 
     gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
     gamerCamila.alugaJogo(jogoSpider, periodo2)
@@ -43,4 +43,7 @@ fun main() {
     gamerCamila.recomendar(10)
     gamerCamila.recomendar(8)
     println(gamerCamila)
+
+    gamerCamila.alugaJogo(jogoResidentVillage, periodo1)
+    println(gamerCamila.jogosAlugados)
 }
