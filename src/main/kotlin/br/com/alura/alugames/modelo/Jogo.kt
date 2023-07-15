c5dcd7595ecb2154725ec37e7d963069ef96454f
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-07-15 00:20:03 -0300

    Faça como eu fiz: vamos praticar?

--- src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Jogo.kt
@@ -1,9 +1,17 @@
 package br.com.alura.alugames.modelo
 
 data class Jogo(val titulo:String,
-                val capa:String) {
+                val capa:String): Recomendavel {
     var descricao: String? = null
     var preco = 0.0
+    private val listaNotas = mutableListOf<Int>()
+    override val media: Double
+        get() = listaNotas.average()
+
+    override fun recomendar(nota: Int) {
+        listaNotas.add(nota)
+    }
+
     constructor(titulo: String, capa: String, preco: Double, descricao: String):
             this(titulo, capa) {
         this.preco = preco
